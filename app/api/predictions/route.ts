59cf03ac10b874aa4e99bb157ff8aa55037cc20d
Miles Johnson <bluehaakonjohnson@gmail.com>
2024-12-31 17:24:48 +1100

    changed route.ts to try and fix JSON parse error. Need to update dev as well

--- app/api/predictions/route.ts
+++ app/api/predictions/route.ts
@@ -1,45 +1,42 @@
 import Replicate from "replicate";
+import { NextRequest, NextResponse } from 'next/server';
 
 const replicate = new Replicate({
-    auth: process.env.REPLICATE_API_TOKEN,
+  auth: process.env.REPLICATE_API_TOKEN,
 });
 
-export async function POST(req: Request) {
+
+export async function POST(req: NextRequest) {
   if (!process.env.REPLICATE_API_TOKEN) {
-    throw new Error(
-      "The REPLICATE_API_TOKEN environment variable is not set. See README.md for instructions on how to set it."
-    );
+     return NextResponse.json({ detail: "The REPLICATE_API_TOKEN environment variable is not set. See README.md for instructions on how to set it." }, { status: 500 })
   }
 
-  const data = await req.formData();
+  try {
+        const data = await req.formData();
+        const prompt = data.get("prompt")?.toString() || "optical illusion 3D maze box creative genius unreal engine black and gray, and colorful magic and spells at the background";
+        const image = data.get("image");
 
-  const prompt = data.get("prompt")?.toString() || "optical illusion 3D maze box creative genius unreal engine black and gray, and colorful magic and spells at the background"
-  const image = data.get("image")
-  const input:any = {
-      seed: -1,
-      prompt,
-      output_format: "png",
-      negative_prompt: "blurry, horror, nsfw",
-    }
-  if(image) {
-    input.image = image
-  }
-    try {
-        const prediction = await replicate.predictions.create({
-            version: "d9243e828737bd0ce73e8cb95f81cead59dead23a303445e676147f02d6121cb",
-            input,
-            // Note: the version key corresponds to "miles-johnson/qr-code-generator:d9243e828737bd0ce73e8cb95f81cead59dead23a303445e676147f02d6121cb"
-            // See https://replicate.com/miles-johnson/qr-code-generator
-        });
+        const input:any = {
+            seed: -1,
+            prompt,
+            output_format: "png",
+            negative_prompt: "blurry, horror, nsfw",
+        };
 
-        if (prediction?.error) {
-            return new Response(JSON.stringify({ detail: prediction.error}), {
-                status: 500,
-            });
+       if (image && image instanceof File) {
+          input.image = image;
         }
 
-        return new Response(JSON.stringify(prediction), { status: 201 });
-    } catch(error: any) {
-       return new Response(JSON.stringify({detail: error.message}), {status: 500})
+    const prediction = await replicate.predictions.create({
+        version: "d9243e828737bd0ce73e8cb95f81cead59dead23a303445e676147f02d6121cb",
+        input,
+    });
+
+    if (prediction?.error) {
+       return NextResponse.json({ detail: prediction.error }, { status: 500 });
     }
+    return NextResponse.json(prediction, { status: 201 });
+  } catch (error:any) {
+      return NextResponse.json({detail: error.message}, {status: 500})
+  }
 }
